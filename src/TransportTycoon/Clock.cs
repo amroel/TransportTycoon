1cf9752c21c5c5c3e0266c6ec472800202f81996
Amro El-Fakharany <amro00765@outlook.com>
2019-11-25 17:17:17 +0100

    removed unused method

--- src/TransportTycoon/Clock.cs
+++ src/TransportTycoon/Clock.cs
@@ -8,8 +8,6 @@ namespace TransportTycoon
 
 		public Clock(int tickInterval) => _tickInterval = tickInterval;
 
-		public void Reset() => ElapsedTime = 0;
-
 		public void Tick() => ElapsedTime += _tickInterval;
 
 		public int ElapsedTime { get; private set; }
